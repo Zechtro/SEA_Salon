bfc115e01a22f7dbc87c6485385a01e4b413def0
Steven <98434141+Zechtro@users.noreply.github.com>
2024-06-22 23:01:49 +0700

    feat: review swiper and simple review form

---
+++ src/app/app/components/ButtonFormReview.tsx
@@ -0,0 +1,13 @@
+import { forwardRef } from "react";
+
+export const Button = forwardRef<HTMLButtonElement, React.ButtonHTMLAttributes<HTMLButtonElement>>((props, ref) => {
+    return (
+        <button
+        {...props}
+        ref={ref}
+        className="flex w-[30vw] h-[8vh] justify-center items-center rounded-lg btn btn-accent text-[2vw] font-semibold leading-6 text-white shadow-sm"
+        />
+  );
+});
+
+Button.displayName = 'Button';
--- src/app/app/routes/_index.tsx
+++ src/app/app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
 import { SalonSlogan, Contact1Name, Contact1Number, Contact2Name, Contact2Number, Service1, Service2, Service3, Service4, Service5, Service1_img , Service2_img, Service3_img, Service4_img, Service5_img, SalonName } from "../components/SalonStaticVar";
 import type { LoaderFunction } from "@remix-run/node";
 import { json, useLoaderData } from "@remix-run/react";
@@ -7,11 +7,30 @@ import 'swiper/swiper-bundle.css';
 import { Navigation, Pagination } from 'swiper/modules';
 import { FaStar } from "react-icons/fa"
 import { useState } from "react";
+import { Form } from "@remix-run/react";
+import { Button } from "../components/ButtonFormReview";
 
 interface Service {
   image_url: string;
   service_name: string;
 }
+
+interface Review {
+  nama_user: string;
+  rating: number;
+  comment: string;
+}
+
+const reviews: Review[] = [
+  {nama_user: "UserDummy1", rating: 5, comment: "GG BANG"},
+  {nama_user: "UserDummy2", rating: 3, comment: "GACORRRRR"},
+  {nama_user: "UserDummy3", rating: 1, comment: "HAHHHH ??!?!?! JADI DUTA SALON LAIN?!?!?! EHEHAHAHAH"},
+  {nama_user: "UserDummy4", rating: 2, comment: "Rill khhh"},
+  {nama_user: "UserDummy5", rating: 4, comment: "Seriusss??"},
+  {nama_user: "UserDummy6", rating: 5, comment: "Isshhh panjang kali lah Isshhh panjang kali lah Isshhh panjang kali lah Isshhh panjang kali lah Isshhh panjang kali lah Isshhh panjang kali lah Isshhh panjang kali lah Isshhh panjang kali lah Isshhh panjang kali lah Isshhh panjang kali lah Isshhh panjang kali lah"},
+  {nama_user: "UserDummy7", rating: 1, comment: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXX"},
+  {nama_user: "UserDummy8", rating: 5, comment: "GG BANG"},
+];
 
 export const loader: LoaderFunction = async () => {
   const services: Service[] = [
@@ -21,7 +40,20 @@ export const loader: LoaderFunction = async () => {
     {image_url: Service4_img, service_name: Service4},
     {image_url: Service5_img, service_name: Service5},
   ];
-  return json({ services })
+
+  return json({ services, reviews });
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  if(formData.get("rating") !== null && !formData.get("comment") !== null){
+    const rating: number = parseInt(formData.get("rating") as string);
+    const comment: string = formData.get("comment") as string;
+    console.log(formData.get("rating"));
+    console.log(formData.get("comment"));
+    reviews.push({nama_user: "UserX", rating: rating, comment: comment});
+  }
+  return null;
 }
 
 export const meta: MetaFunction = () => {
@@ -34,7 +66,7 @@ export const meta: MetaFunction = () => {
 
 
 export default function Index() {
-  const { services } = useLoaderData<typeof loader>();
+  const { services, reviews } = useLoaderData<{ services: Service[], reviews: Review[] }>();
   const [rating, setRating] = useState(0);
 
   const handleClick = (index: number) => {
@@ -103,35 +135,102 @@ export default function Index() {
       </section>
 
       {/* REVIEW LIST SECTION */}
-      <section>
-
+      <section className="mt-[10vh] z-20 pl-4 pr-4">
+      <h2 className="h2 flex justify-center">Reviews</h2>
+        <Swiper
+          spaceBetween={0}
+          pagination={{ 
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          grid={{
+            rows: 1,
+          }}
+          breakpoints={{
+            360: {
+              slidesPerView: 1,
+            },
+            1200: {
+              slidesPerView: 2,
+            },
+            1500: {
+              slidesPerView: 3
+            }
+          }}
+          className="w-[90vw] h-[48vh]"
+        >
+          {reviews.map((review: Review) => (
+            <SwiperSlide key={review.nama_user} className="flex flex-row justify-around items-center">
+              <div className="flex justify-center items-center sm:w-[70vw] xl:w-[40vw] 2xl:w-[25vw] h-[40vh] bg-white rounded-xl border-[0.2vw] border-accent">
+                <div className="flex flex-col justify-around items-start sm:w-[65vw]  xl:w-[35vw] 2xl:w-[20vw] h-[35vh]">
+                  <div>
+                    <h3 className="h3">{review.nama_user}</h3>
+                    <div className="flex flex-row w-full justify-start">
+                      <div className="flex justify-start">
+                        {[...Array(5)].map((_, index) => (
+                          <FaStar
+                          key={index}
+                          size={20}
+                          color={index < (review.rating) ? 'gold' : 'lightgray'}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <p className="break-all overflow-y-auto h-[20vh]">
+                    {review.comment}
+                  </p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+          
+        </Swiper>
       </section>
 
       {/* REVIEW FORM SECTION */}
       <section>
-        <h2 className="h2 flex justify-center mt-[3vh]">Review Us!</h2>
-        <div className="flex mt-[5vh] w-full justify-center">
-          <div className="flex w-[40vw] justify-around">
-            {[...Array(5)].map((_, index) => (
-              <FaStar
-              key={index}
-              size={70}
-              color={index < (rating) ? 'gold' : 'lightgray'}
-              onClick={() => handleClick(index + 1)}
-              style={{ cursor: 'pointer' }}
-              />
-            ))}
+        <h2 className="h2 flex justify-center mt-[5vh]">Review Us!</h2>
+        <Form method="post" className="flex flex-col ">
+          <div className="flex flex-row mt-[2vh] w-full justify-center">
+            <div className="flex w-[40vw] justify-around">
+              {[...Array(5)].map((_, index) => (
+                <FaStar
+                key={index}
+                size={70}
+                color={index < (rating) ? 'gold' : 'lightgray'}
+                onClick={() => handleClick(index + 1)}
+                style={{ cursor: 'pointer' }}
+                />
+              ))}
+            </div>
+            <input name="rating" type="number" hidden value={rating} />
           </div>
-        </div>
+
+          <div className="mt-[3vh] flex justify-center">
+            <textarea
+                name="comment"
+                rows={4}
+                placeholder="Share your experience"
+                className="resize-none flex items-start sm:w-[70vw] lg:w-[50%] border-[0.2vw] border-accent rounded-[1vw] p-4 h-[45vh]"
+                required
+            ></textarea>
+          </div>
+
+          <div className="flex w-full justify-center mt-[3vh]">
+            <Button type="submit">Send Review</Button>
+          </div>
+        </Form >
       </section>
 
       {/* FOOTER */}
       <footer className="pt-[5vh] pb-[5vh] bg-accent mt-[10vh] relative z-20">
-        <div className="container mx-auto px-0 flex flex-col items-center justify-center">
+        <div className="container mx-auto px-0 flex flex-col items-center justify-between">
           <div className="flex flex-col xl:flex-row justify-around items-center w-full">
             <div className="flex flex-col justify-around xl:justify-start items-center xl:items-start">
               <h2 className="h2">{SalonName}</h2>
-              <p>{SalonSlogan}</p>
+              <p><em>{SalonSlogan}</em></p>
               <br></br>
             </div>
             <div className="flex flex-col justify-around xl:justify-start items-center xl:items-start">
@@ -140,8 +239,7 @@ export default function Index() {
               <p>{Contact2Name} : {Contact2Number}</p>
             </div>
           </div>
-          <br></br>
-          <p>Copyright &copy; SEA Salon 2024. All rights reserved.</p>
+          <p className="mt-[10vh] text-[2vh]">Copyright &copy; SEA Salon 2024. All rights reserved.</p>
         </div>
       </footer>
 
